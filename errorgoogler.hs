b3c808ac144a6f1e2007d46a2ed07761b3cdf173
Bradley Morrell <brrmorre@gmail.com>
2020-08-17 16:33:43 -0700

    Update errorgoogler.hs

--- errorgoogler.hs
+++ errorgoogler.hs
@@ -1,3 +1,10 @@
 compilererrorgoogler :: String -> String -> String -- InputFile -> ErrorMessage -> OutputFile
+--google the error
+--go on stack overflow
+--apply fix to inputfile
+--output fixed file
 
 oserrorgoogler :: String -> String -- ErrorMessage -> String of bash commands
+--google the error
+--go on on a forum and get the answer
+--output the answer
