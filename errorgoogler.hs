3876e511ce06b785ed2d48c441b7c9496a855db3
Bradley Morrell <brrmorre@gmail.com>
2020-08-18 02:54:54 -0700

    Update errorgoogler.hs

--- errorgoogler.hs
+++ errorgoogler.hs
@@ -3,7 +3,6 @@
 -- mike was here
 import System.Process
 import Text.Regex.TDFA
-import Text.Regex.TDFA.Text ()
 
 wget url = createProcess (proc "wget" ["-U", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "-q", url, "-O", "-"]) -- should dump the html from the url
 whiteSpaceToPlusConverter string = map (\character -> if character==' ' then '+'; else character) string
