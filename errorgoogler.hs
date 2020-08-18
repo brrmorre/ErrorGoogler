f69c586e9b7025c173774855448992544ab29bf2
Bradley Morrell <brrmorre@gmail.com>
2020-08-17 19:21:54 -0700

    Update errorgoogler.hs

--- errorgoogler.hs
+++ errorgoogler.hs
@@ -5,7 +5,7 @@ import System.Process
 
 googlescraper :: String -> [String] -- take a string and google it and then give relevant urls/relevant text from the html
 
-wget url = createProcess (proc "wget" ["-q", "url", "-O", "-"]) -- should dump the html from the url
+wget url = createProcess (proc "wget" ["-q", url, "-O", "-"]) -- should dump the html from the url
 
 googledesugarer -- take in a text file(string) and replace google searches with googlescraper(google search)
 htmlresearcher -- take in a url and research string and output relevant text from the url
