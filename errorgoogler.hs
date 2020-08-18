9ac30e7dd499b5dad952278dd002a29350548386
Bradley Morrell <brrmorre@gmail.com>
2020-08-17 18:50:23 -0700

    Update errorgoogler.hs

--- errorgoogler.hs
+++ errorgoogler.hs
@@ -16,6 +16,7 @@ google :: String -> [String] --google the error
 --https://hackage.haskell.org/package/scalpel-0.6.2/src/src/Text/HTML/Scalpel.hs
 --https://hackage.haskell.org/package/http-wget
 --https://hackage.haskell.org/package/scalpel-0.6.2/src/src/Text/HTML/Scalpel/Internal/Scrape/URL.hs
+--https://hackage.haskell.org/package/http-conduit
 --go on stack overflow
 --apply fix to inputfile
 --output fixed file
