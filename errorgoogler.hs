13f4e14650de15ddfb0b532b71df93cf51c1c405
Bradley Morrell <brrmorre@gmail.com>
2020-08-18 17:40:49 -0700

    Update errorgoogler.hs

--- errorgoogler.hs
+++ errorgoogler.hs
@@ -20,6 +20,7 @@ main = do
         (_, Just ddghout, _, _) <- duckduckgoSearcher(errorMessage)
         searchResultHTML <- hGetContents ddghout
         putStrLn searchResultHTML
+        putStrLn errorMessage
 
 wget url = createProcess (proc "wget" ["-U", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "-q", url, "-O", "-"]){ std_out = CreatePipe } -- should dump the html from the url
 --wget url = createProcess (proc "wget" ["-U", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "-q", url, "-o", "search.html"]) -- should dump the html from the url
